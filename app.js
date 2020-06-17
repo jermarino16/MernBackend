bfc297bacf44e4faa4181c3fbcc6e6fb8ad55e40
Jeremy Marino <jermarino16@gmail.com>
2020-06-17 10:31:51 -0700

    api routes for users

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesroutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/user-routes");
 const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use("/api/places", placesroutes);
+app.use("/api/users", usersRoutes);
 app.use((req, res, next) => {
   const error = new HttpError("Could not find this route.", 404);
   throw error;
