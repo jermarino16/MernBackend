c787d81ddff620db97fe2dcc714204ffe942f60c
Jeremy Marino <jermarino16@gmail.com>
2020-06-17 08:27:43 -0700

    created httperror model, made controllers for the basic routes to keep routes file clean

---
+++ controllers/places-controllers.js
@@ -0,0 +1,58 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State building",
+    description: "One of the most famous sky scrapers in the world1",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New Yorkk, NY 10001",
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "My House",
+    description: "Comfy Area",
+    location: {
+      lat: 0.0,
+      lng: 0.0,
+    },
+    address: "Center of the Earth",
+    creator: "u1",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404);
+  }
+
+  res.json({ place });
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const userPlaces = DUMMY_PLACES.filter((u) => {
+    return u.creator === userId;
+  });
+
+  if (userPlaces.length === 0) {
+    return next(
+      new HttpError("Could not find a place for the provider user id.", 404)
+    );
+  }
+
+  res.json({ userPlaces });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,62 +1,11 @@
 const express = require("express");
-const { response } = require("express");
+
+const placesControllers = require("../controllers/places-controllers");
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State building",
-    description: "One of the most famous sky scrapers in the world1",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New Yorkk, NY 10001",
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "My House",
-    description: "Comfy Area",
-    location: {
-      lat: 0.0,
-      lng: 0.0,
-    },
-    address: "Center of the Earth",
-    creator: "u1",
-  },
-];
+router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
-
-  if (!place) {
-    const error = new Error("Could not find a place for the provided id.");
-    error.code = 404;
-    throw error;
-  }
-
-  res.json({ place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const userPlaces = DUMMY_PLACES.filter((u) => {
-    return u.creator === userId;
-  });
-
-  if (userPlaces.length === 0) {
-    const error = new Error("Could not find a place for the provided user id.");
-    error.code = 404;
-    return next(error);
-  }
-
-  res.json({ userPlaces });
-});
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
 module.exports = router;
