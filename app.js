900cf61f8acec83f9958a50f597dcb870b5b3311
Jeremy Marino <jermarino16@gmail.com>
2020-06-17 09:18:31 -0700

    support for unsupported routes

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesroutes = require("./routes/places-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use("/api/places", placesroutes);
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
