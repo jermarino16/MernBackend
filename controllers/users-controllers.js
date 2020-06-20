a2f2374e9c28f9bdddf2335f3075360b55623043
Jeremy Marino <jermarino16@gmail.com>
2020-06-20 09:15:23 -0700

    find places using user model instead of palces model

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,6 +1,5 @@
 const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
-const mongoose = require("mongoose");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
